1298134190554fbb56390ac114a0fb777e2f88c8
hathawayjess <hathaway.jess@gmail.com>
2016-09-08 17:08:34 -0700

    updating tests

---
+++ app.js
@@ -0,0 +1,55 @@
+angular.module('embed', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('single',{
+    url:'/',
+    templateUrl: "./templates/single.html"
+  })
+  .state('multiple', {
+  	url:'/multiple',
+  	templateUrl: "./templates/multiple.html"
+  })
+  .state('video', {
+  	url:'/video',
+  	templateUrl: "./templates/video.html"
+  })
+  .state('audio', {
+  	url:'/audio',
+  	templateUrl: "./templates/audio.html"
+  })
+  .state('pdf', {
+  	url:'/pdf',
+  	templateUrl: "./templates/pdf.html"
+  })
+
+  $urlRouterProvider
+  .otherwise('/');
+})
+
+.directive('script', function() {
+    return {
+      restrict: 'E',
+      scope: false,
+      link: function(scope, elem, attr) 
+      {
+        if (attr.type==='text/javascript-lazy') 
+        {
+          var s = document.createElement("script");
+          s.type = "text/javascript";                
+          var src = elem.attr('src');
+          if(src!==undefined)
+          {
+              s.src = src;
+          }
+          else
+          {
+              var code = elem.text();
+              s.text = code;
+          }
+          document.head.appendChild(s);
+          elem.remove();
+        }
+      }
+    };
+  });
+ 
